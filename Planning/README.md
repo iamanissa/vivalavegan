d2c03f01b37473eb9f93ed1af2e1303417260fa0
winishasmith <winishasmith@gmail.com>
2016-04-01 16:10:28 -0400

    add planning folder that includes readme, wireframe folder, and ERD folder

---
+++ Planning/README.md
@@ -0,0 +1,33 @@
+# Bronze
+
+- User be able to go to index page and see all restaurants that have vegan options.
+
+- User should be able to click on restaurant and go to show page that has available vegan options.  
+
+- Restaurant show page should display vegan menu options for that restaurant.  
+
+- Tags for standard or custom menu option, or pull it out your ass
+
+# Silver
+
+- User should be able to add, delete, and update a restaurant in the restaurant index
+
+- User should be able to add, delete, and update a food to a restaurant
+
+- User should be able to search/filter through index of restaurants.
+
+- User should be able to search/filter through all of a restaurant’s foods.  
+
+# Gold
+
+- Travel section - When you’re in this city, this place has good vegan options.  
+
+- Comments, Ratings, Stars
+
+- Many to many: foods to restaurants
+
+# Platinum
+
+- Users, Sessions, Authentication,
+
+- Different food restrictions
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,11 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 Restaurant.destroy_all
+Food.destroy_all
 
 Restaurant.create(name: "Johnny Rockets", info: "Johnny Rockets is an American restaurant franchise whose themed decor is based upon 1950s diner-style restaurants.", category: "American", photo_url: "https://upload.wikimedia.org/wikipedia/en/0/0f/Johnny_Rockets_logo.png")
 Restaurant.create(name: "Taco Bell", info: "Taco Bell serves a variety of Tex-Mex foods including tacos, burritos, quesadillas, nachos, other specialty items", category: "Mexican", photo_url: "http://goodlogo.com/images/logos/taco_bell_logo_2934.gif")
 Restaurant.create(name: "Burger King", info: "Burger King, often abbreviated as BK, is an American global chain of hamburger fast food restaurants.", category: "American", photo_url: "http://3.bp.blogspot.com/-kwZJdPLmoSg/Tqr6q2upUXI/AAAAAAAACUc/j2jNOlxAXI0/s1600/Burger+King+Logo.jpg")
-Restaurant.create(name: "IHOP", info: "International House of Pancakes, usually known as IHOP is a United States-based family restaurant chain that specializes in breakfast foods.", category: "American", photo_url: "http://www.wired.com/wp-content/uploads/2015/06/ihope-orig-inline.jpg")
+ihop = Restaurant.create(name: "IHOP", info: "International House of Pancakes, usually known as IHOP is a United States-based family restaurant chain that specializes in breakfast foods.", category: "American", photo_url: "http://www.wired.com/wp-content/uploads/2015/06/ihope-orig-inline.jpg")
+
+Food.create(name: "hash browns", ingredients: "potatoes", tag: "American", image_url: "https://upload.wikimedia.org/wikipedia/commons/9/98/Kartoffelpuffer.jpg", restaurant: ihop)

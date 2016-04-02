cc036efc0fd6eeb7932f295da191bc946123e20a
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-04-02 08:28:47 -0400

    adds new api controller action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -3,4 +3,15 @@ class RestaurantsController < ApplicationController
     @restaurants = Restaurant.all
     # render status: 200, json: @restaurants.to_json
   end
+
+def show
+end
+
+def api
+  @restaurants = Restaurant.all
+  render status: 200, json: @restaurants.to_json
+end
+
+
+
 end

300fd4b74245a84002ee2a86861f0e7f6de76b3d
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-04-02 11:05:53 -0400

    removes show action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,17 +1,12 @@
 class RestaurantsController < ApplicationController
+
   def index
-    @restaurants = Restaurant.all
-    # render status: 200, json: @restaurants.to_json
+
   end
 
-def show
-end
-
-def api
-  @restaurants = Restaurant.all
-  render status: 200, json: @restaurants.to_json
-end
-
-
+  def api
+    @restaurants = Restaurant.all
+    render status: 200, json: @restaurants.to_json
+  end
 
 end

f7b348e5134506756e107b26b29ca492fb06c346
Anissa Jones <anissajones10@gmail.com>
2016-04-03 12:35:24 -0400

    Attempts to create controller for panels to put panel logic in a controller.

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -58,5 +58,14 @@
         }
       });
     }
-  ]);
+  ])
+  // .controller("PanelController", function(){
+  //   this.tab = 1;
+  //   this.selectTab = function(setTab){
+  //     this.tab = setTab;
+  //   };
+  //   this.isSelected = function(checkTab){
+  //     return this.tab === checkTab;
+  //   };
+  // });
 })();

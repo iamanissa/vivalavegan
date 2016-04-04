6007c9a7cefed54fdea89e42bb5196cdb52e0cc2
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-04-04 11:01:17 -0400

    adds correct params to index and show action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,13 +1,15 @@
 class FoodsController < ApplicationController
 
   def index
-    @foods = Food.all
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @foods = @restaurant.foods
     render status: 200, json: @foods.to_json
   end
 
-def show
-  @food = Food.find(params[:id])
-  render status: 200, json: @food.to_json
-end
+  def show
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @food = @restaurant.foods.find(params[:id])
+    render status: 200, json: @food.to_json
+  end
 
 end

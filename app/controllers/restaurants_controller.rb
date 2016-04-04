f8fca567a9d082671990f7acdd5ae4d8bca91a7d
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-04-04 09:31:35 -0400

    adds show action

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,18 @@
 class RestaurantsController < ApplicationController
 
   def index
+    @restaurants = Restaurant.all
+    render status: 200, json: @restaurants.to_json
+  end
+
+  def show
+
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    render status: 200, json: @restaurant.to_json
 
   end
+
+
 
   def api
     @restaurants = Restaurant.all

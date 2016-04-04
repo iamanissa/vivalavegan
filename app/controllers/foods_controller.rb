2b53fc0dbf3b98d29678f4c3e4490a72241a0b08
Matthew Stevens <matthew.lee.stevens@gmail.com>
2016-04-04 10:26:28 -0400

    adds show action

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,6 +1,13 @@
 class FoodsController < ApplicationController
+
   def index
     @foods = Food.all
     render status: 200, json: @foods.to_json
   end
+
+def show
+  @food = Food.find(params[:id])
+  render status: 200, json: @food.to_json
+end
+
 end
